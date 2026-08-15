221e41eafbe82fb60930742cd10f1e5ffa15dd26
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-08-15 20:51:31 +0800

    update

---
+++ hello.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello, World!", 0xA
+    msg_len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4          ; sys_write
+    mov ebx, 1           ; stdout
+    mov ecx, msg
+    mov edx, msg_len
+    int 0x80
+
+    mov eax, 1           ; sys_exit
+    mov ebx, 0
+    int 0x80
