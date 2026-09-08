d23d02e74a0a0a410785b0b0230f9dd382b5d290
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-09-08 17:32:24 +0800

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -1,17 +1,21 @@
+;ascii.cl
+
+;code->sys_write->Linux Kernel -> terminal 
+
 section .data
-    msg db "Hello, World!", 0xA
-    msg_len equ $ - msg
+    msg db "Hello, World!", 0xA ;A - LF \n
+    msg_len equ $ - msg     ;len function - 13
 
 section .text
     global _start
 
 _start:
     mov eax, 4          ; sys_write
-    mov ebx, 1           ; stdout
-    mov ecx, msg
-    mov edx, msg_len
-    int 0x80
+    mov ebx, 1          ; stdout
+    mov ecx, msg        ;message to display 'Hello World!'
+    mov edx, msg_len    ;13
+    int 0x80            ;int
 
     mov eax, 1           ; sys_exit
-    mov ebx, 0
+    mov ebx, 0           ; return 0;
     int 0x80
